2958fa25d1c75b0580ae406052205bf78160ad48
Byeonggon Lee <gonny952@gmail.com>
2019-10-02 20:36:03 +0900

    Add comments

--- gonapps.cpp
+++ gonapps.cpp
@@ -2,17 +2,24 @@
 #include "inf_int.h"
 
 friend inf_int operator+(const inf_int& lhs, const inf_int& rhs) {
+	// + +: len == longer_len || len == longer_len + 1
+	// - -: len == longer_len || len == longer_len + 1
+	// + -: 1 <= len <= longer_len
+
+	return res;
 }
 
 friend inf_int operator-(const inf_int& lhs, const inf_int& rhs) {
-	
+	return res;
 }
 
 friend inf_int operator*(const inf_int& lhs, const inf_int& rhs) {
-	
+	// len == len1 + len2 || len == len1 + len2 + 1
+	return res;
 }
 
 friend inf_int operator/(const inf_int& lhs, const inf_int& rhs) {
+	return res;
 }
 
 friend std::ostream& operator<<(std::ostream& ostr, const inf_int& self) {
