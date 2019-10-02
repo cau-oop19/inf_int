b46101bec4a1674c1fab5c5fcb836cd652d39afd
Byeonggon Lee <gonny952@gmail.com>
2019-10-02 20:24:40 +0900

    Revert "Add inf_int::resize()"
    This reverts commit 88bd0d10a0f638295ec65b45885eac0ff1d96927.

--- gonapps.cpp
+++ gonapps.cpp
@@ -4,18 +4,14 @@
 friend inf_int operator+(const inf_int& lhs, const inf_int& rhs) {
 	
 }
-
 friend inf_int operator-(const inf_int& lhs, const inf_int& rhs) {
 	
 }
-
 friend inf_int operator*(const inf_int& lhs, const inf_int& rhs) {
 	
 }
-
 friend inf_int operator/(const inf_int& lhs, const inf_int& rhs) {
 }
-
 friend std::ostream& operator<<(std::ostream& ostr, const inf_int& self) {
 	std::string str{digits, length};
 	ostr << std::move((thesign ? "+", "-") + str);
