ae0c35cb3586dee5d86aa8c324e6b4468b82e4cc
Byeonggon Lee <gonny952@gmail.com>
2019-10-02 20:26:10 +0900

    Enhance operator<<

--- gonapps.cpp
+++ gonapps.cpp
@@ -2,19 +2,23 @@
 #include "inf_int.h"
 
 friend inf_int operator+(const inf_int& lhs, const inf_int& rhs) {
-	
 }
+
 friend inf_int operator-(const inf_int& lhs, const inf_int& rhs) {
 	
 }
+
 friend inf_int operator*(const inf_int& lhs, const inf_int& rhs) {
 	
 }
+
 friend inf_int operator/(const inf_int& lhs, const inf_int& rhs) {
 }
+
 friend std::ostream& operator<<(std::ostream& ostr, const inf_int& self) {
 	std::string str{digits, length};
-	ostr << std::move((thesign ? "+", "-") + str);
+	ostr << thesign ? "+", "-";
+        ostr << std::move(str);
 	return ostr;
 }
 
