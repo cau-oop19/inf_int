10fb14cba2e382fadb61d08ab59c6261bf0b6779
Byeonggon Lee <gonny952@gmail.com>
2019-10-02 20:16:27 +0900

    Implement operator<<

---
+++ gonapps.cpp
@@ -0,0 +1,20 @@
+#include <utility>
+#include "inf_int.h"
+
+friend inf_int operator+(const inf_int& lhs, const inf_int& rhs) {
+	
+}
+friend inf_int operator-(const inf_int& lhs, const inf_int& rhs) {
+	
+}
+friend inf_int operator*(const inf_int& lhs, const inf_int& rhs) {
+	
+}
+friend inf_int operator/(const inf_int& lhs, const inf_int& rhs) {
+}
+friend std::ostream& operator<<(std::ostream& ostr, const inf_int& self) {
+	std::string str{digits, length};
+	ostr << std::move((thesign ? "+", "-") + str);
+	return ostr;
+}
+
