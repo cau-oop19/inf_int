aba82307c775be2b54ab471eea892160aa2e9998
Byeonggon Lee <gonny952@gmail.com>
2019-10-03 22:02:28 +0900

    Fix longer shorter bug

--- gonapps.cpp
+++ gonapps.cpp
@@ -64,7 +64,7 @@ inf_int operator+(const inf_int& lhs, const inf_int& rhs) {
 	// - -: len == longer_len || len == longer_len + 1
 	// + -: 1 <= len <= longer_len
 	const auto& longer = lhs.length > rhs.length ? lhs : rhs;
-	const auto& shorter = lhs.length < rhs.length ? lhs : rhs;
+	const auto& shorter = lhs.length > rhs.length ? rhs : lhs;
 	const auto longer_sign = static_cast<uint8_t>(longer.thesign ? 1 : -1);
 	const auto shorter_sign = static_cast<uint8_t>(shorter.thesign ? 1 : -1);
 	inf_int res{longer.length};
@@ -89,7 +89,7 @@ inf_int operator-(const inf_int& lhs, const inf_int& rhs) {
 inf_int operator*(const inf_int& lhs, const inf_int& rhs) {
 	//  len == len1 + len2 - 1 || len == len1 + len2
 	const auto& longer = lhs.length > rhs.length ? lhs : rhs;
-	const auto& shorter = lhs.length < rhs.length ? lhs : rhs;
+	const auto& shorter = lhs.length > rhs.length ? rhs : lhs;
 	auto res = longer;
 	for(size_t i = 0; i != shorter.length - 1; ++i) {
 		for(size_t j = 0; j != longer.length - 1; ++j) {
