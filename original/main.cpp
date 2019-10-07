445206ba9401647dc9df9963dfe1468872482e02
Byeonggon Lee <gonny952@gmail.com>
2019-10-07 22:09:12 +0900

    Fix comments

--- original/main.cpp
+++ original/main.cpp
@@ -24,13 +24,13 @@ int main()
 	/////////////////////////////
 	assert((inf_int(100) + inf_int(200)) == inf_int(300));	//양수 + 양수
 
-	assert((inf_int(100) + inf_int(-200)) == inf_int(-100));	//양수 + 음수
+	assert((inf_int(100) + inf_int(-200)) == inf_int(-100)); //양수 + 음수
 
-	assert((inf_int(-200) + inf_int(-300)) == inf_int(-500));	//음수 + 음수
+	assert((inf_int(-200) + inf_int(-300)) == inf_int(-500)); //음수 + 음수
 
-	assert((inf_int("80000000000") + inf_int("-30000000000")) == inf_int("50000000000"));	//큰양수 + 큰음수
+	assert((inf_int("80000000000") + inf_int("-30000000000")) == inf_int("50000000000")); //큰양수 + 큰음수
 
-	assert((inf_int("-9999999998") + inf_int("-9999999999")) == inf_int("-19999999997"));	//큰음수 + 큰음수 올림수테스트
+	assert((inf_int("-9999999998") + inf_int("-9999999999")) == inf_int("-19999999997")); //큰음수 + 큰음수 올림수테스트
 
 	/////////////////////////////
 	/*            -            */
@@ -39,24 +39,24 @@ int main()
 
 	assert((inf_int(100) - inf_int(-200)) == inf_int(300));	//양수 - 음수
 
-	assert((inf_int(-200) - inf_int(-300)) == inf_int(100));	//음수 - 음수
+	assert((inf_int(-200) - inf_int(-300)) == inf_int(100)); //음수 - 음수
 
-	assert((inf_int("80000000000") - inf_int("-30000000000")) == inf_int("110000000000"));	//큰양수 - 큰음수
+	assert((inf_int("80000000000") - inf_int("-30000000000")) == inf_int("110000000000")); //큰양수 - 큰음수
 
-	assert((inf_int("-9999999998") - inf_int("-9999999999")) == inf_int(1));	//큰음수 - 큰음수 올림수테스트
+	assert((inf_int("-9999999998") - inf_int("-9999999999")) == inf_int(1)); // 큰음수 - 큰음수 올림수테스트
 
 	/////////////////////////////
 	/*            *            */
 	/////////////////////////////
 	assert((inf_int(100)* inf_int(200)) == inf_int(20000));	//양수 * 양수
 
-	assert((inf_int(100) * inf_int(-200)) == inf_int(-20000));	//양수 * 음수
+	assert((inf_int(100) * inf_int(-200)) == inf_int(-20000)); //양수 * 음수
 
-	assert(inf_int(-200) * inf_int(-300) == inf_int(60000));	//음수 * 음수
+	assert(inf_int(-200) * inf_int(-300) == inf_int(60000)); //음수 * 음수
 
 	assert((inf_int("80000000000")*inf_int("-30000000000")) == inf_int("-2400000000000000000000"));	//큰양수 * 큰음수
 
-	assert((inf_int("-9999999998")*inf_int("9999999999")) == inf_int("-99999999970000000002"));	//큰음수 * 큰음수 올림수테스트
+	assert((inf_int("-9999999998")*inf_int("9999999999")) == inf_int("-99999999970000000002")); //큰음수 * 큰음수 올림수테스트
 	
 	//////////////////////////////
 	/*            ==            */
@@ -128,13 +128,12 @@ int main()
 	inf_int f = d;
 	inf_int g(f);
 
-	// cin >> g ;   // not required
+	// cin >> g; // not required
 
 	a = b * c;
-	// e=g/f;       // not required
+	// e=g/f; // not required
 
 	b = c - d;
-
 
 	if (f == d) {
 		std::cout << "a : " << a << std::endl;
@@ -145,6 +144,5 @@ int main()
 		std::cout << "f : " << f << std::endl;
 		// cout << "g : " << g << endl;
 	}
-
 	return 0;
 }
